6358d4c466ec2a636fb5f22b284c0bb74831caea
nihinivi <48147110+nihinivi@users.noreply.github.com>
2019-08-18 21:04:43 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+#Firstly this is imei verification for revo gg
+
+1) To use it just add your code after the whole code and edit it a bit
---
+++ verification.lua
@@ -0,0 +1,29 @@
+--MADE BY NIHINIVI (seems like this is going to be MDE BY {insertyourname} lol do whatever please leave abit of credits (0w0)
+
+
+
+lul = "1234567890"  --put your imei here
+table = revo.getPhoneInfo()
+function Tut(t, i)
+    local text = "'"       
+    for k,v in pairs(t) do   
+            text = text .. "'" .. k .. "' ==> "    
+            text = text .. v .. "\n\n\n"       
+    end
+    return text
+end
+p = Tut(table ,1)
+f = assert (io.tmpfile ()) 
+f:write (p)  
+f:seek ("set", 1) 
+s = f:read("*a")
+IMEI = s:match "[+-]?%d+"
+if IMEI ~= lul then 
+print("THE IMEI IS INVALID SORRY LOL")
+os.exit()
+else
+print("OK THE IMEI CHECK PASSED")
+--YOUR PEICE OF EXECUTION CODE HERE
+
+
+
